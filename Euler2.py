f78d913bb7cc86fd67416362d0e0247671babdf4
wilsonsamuel407 <63747717+wilsonsamuel407@users.noreply.github.com>
2022-08-27 17:09:53 +0100

    Update Euler2.py

--- Euler2.py
+++ Euler2.py
@@ -1,3 +1,7 @@
+# Even Fibonacci numbers
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+# A Fibonacci Function
 def Fibonacci(n):
 	initsequence = [0,1]
 
@@ -7,5 +11,23 @@ def Fibonacci(n):
 		initsequence.append(next_num)
 	return initsequence
 
-sequence = Fibonacci(10)
-print(sequence)
+# Create a list of values up to 4 million
+for a in range(1,40,1):
+	m = max(Fibonacci(a))
+	if m < 4000000:
+		continue
+	else:
+		break
+
+sequence = Fibonacci(a)
+
+# Create the subset of even numbers
+even_sequence = []
+for seq in sequence:
+	if seq % 2 == 0:
+		even_sequence.append(seq)
+	else:
+		pass
+# Sum the list of even Fibonnaci numbers less than 4 million
+answer = sum(even_sequence)
+print(answer)
